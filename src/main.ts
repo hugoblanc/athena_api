8bcc35bb636cf90f213d2e967004b5cb561d480b
Hugo <hugoblanc.blend@gmail.com>
2019-07-13 10:33:21 +0200

    setup heroku

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@
   async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
-    await app.listen(3000);
+    await app.listen(process.env.PORT || 3000);
 
     admin.initializeApp({
       credential: admin.credential.applicationDefault(),
