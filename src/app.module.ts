2f8dc0c3d2b7389240a1268d9a47bd55f5ab43cb
Hugo <hugoblanc.blend@gmail.com>
2019-07-31 10:29:08 +0200

    Setup - mysql with typeOrm Fix - false positive detect news

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,24 @@ import { PostService } from './providers/post-service';
 import { NotificationService } from './providers/notification-service';
 import { MediaController } from './controllers/media/media.controller';
 import { MediaService } from './providers/media/media.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ScheduleModule.register(), HttpModule],
-  controllers: [AppController, MediaController],
-  providers: [AppService, CronService, ExternalService, PostService, NotificationService, MediaService],
+    imports: [
+        ScheduleModule.register(),
+        HttpModule,
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: process.env.ATHENA_DB_HOST,
+            port: parseInt(process.env.ATHENA_DB_PORT, 10),
+            username: process.env.ATHENA_DB_USER,
+            password: process.env.ATHENA_DB_PASSWORD,
+            database: 'athena',
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: true,
+        }),
+    ],
+    controllers: [AppController, MediaController],
+    providers: [AppService, CronService, ExternalService, PostService, NotificationService, MediaService],
 })
-export class AppModule {}
+export class AppModule { }
