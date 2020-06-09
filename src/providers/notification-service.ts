61a869074938f8b9132572c5d92f852333d0b858
MrWhite <hugoblanc.blend@gmail.com>
2020-06-09 18:21:45 +0200

    fix: forget notification

--- src/providers/notification-service.ts
+++ src/providers/notification-service.ts
@@ -32,13 +32,13 @@ export class NotificationService {
     // On ne peut pas envoyer tout les message car sinon on dupplique les notificaiton ... dommage
     console.log(messages[0]);
 
-    // from(admin.messaging().send(messages[0])).subscribe((resultSend) => {
-    //   this.logger.log('Successfully sent message');
-    //   console.log(messages[0]);
-    // }, (error) => {
-    //   this.logger.error('Error sending message');
-    //   this.logger.error(JSON.stringify(error));
-    // });
+    from(admin.messaging().send(messages[0])).subscribe((resultSend) => {
+      this.logger.log('Successfully sent message');
+      console.log(messages[0]);
+    }, (error) => {
+      this.logger.error('Error sending message');
+      this.logger.error(JSON.stringify(error));
+    });
   }
 
   /**
