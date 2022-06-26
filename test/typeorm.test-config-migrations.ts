26e203ff9405b57f3269ca4e93402ddc8a44df32
Hugo Blanc <hugo.blanc@agicap.com>
2022-06-26 17:59:44 +0200

    fix: drop schema and recreate on each startup

--- test/typeorm.test-config-migrations.ts
+++ test/typeorm.test-config-migrations.ts
@@ -3,6 +3,7 @@ import { testDatabaseName, typeormCOnfig } from '../src/core/configuration/typeo
 export = {
   ...typeormCOnfig,
   autoLoadEntities: false,
+  dropSchema: true,
   database: testDatabaseName,
   entities: ['src/**/*.entity{.ts,.js}'],
 };
