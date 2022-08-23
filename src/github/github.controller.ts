47eb9e22bba01227111e23c0c28beddf91fc2ac2
Hugo Blanc <hugo.blanc@agicap.com>
2022-08-23 20:01:11 +0200

    fix: github root path removed from last ios version

--- src/github/github.controller.ts
+++ src/github/github.controller.ts
@@ -3,11 +3,38 @@ import { GithubService } from './github.service';
 import { Issue } from './issue';
 import { Observable } from 'rxjs';
 
-@Controller('github')
+@Controller()
 export class GithubController {
   private static BASE_URL = 'issues';
   private readonly logger = new Logger(GithubController.name);
   constructor(private githubService: GithubService) { }
+
+  /**
+   * @deprecated The method should not be used
+   */
+  @Get('github/'+GithubController.BASE_URL)
+  getIssueLegacy(@Body() issue: Issue): Observable<Issue> {
+    this.logger.log('Post Issue');
+    return this.githubService.postIssue(issue);
+  }
+
+  /**
+   * @deprecated The method should not be used
+   */
+  @Post('github/'+GithubController.BASE_URL)
+  postIssueLegacy(@Body() issue: Issue): Observable<Issue> {
+    this.logger.log('Post Issue');
+    return this.githubService.postIssue(issue);
+  }
+
+  /**
+   * @deprecated The method should not be used
+   */
+  @Post('github/'+GithubController.BASE_URL + '/:issueId/clap')
+  clapIssueLegacy(@Param('issueId') issueId: string): Observable<Issue> {
+    this.logger.log('Clap issue' + issueId);
+    return this.githubService.clapIssue(issueId);
+  }
 
   @Get(GithubController.BASE_URL)
   getIssue(@Body() issue: Issue): Observable<Issue> {
@@ -26,4 +53,6 @@ export class GithubController {
     this.logger.log('Clap issue' + issueId);
     return this.githubService.clapIssue(issueId);
   }
+
+
 }
